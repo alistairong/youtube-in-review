5153bb7dc9619ad066c7a48fa0e1f83b24c00c7a
Haozhe <hzthegreat@gmail.com>
2020-01-18 19:21:58 +0800

    second init

---
+++ config.js
@@ -0,0 +1,23 @@
+const port = 3002;
+const baseURL = `http://localhost:${port}`;
+module.exports = {
+  // The secret for the encryption of the jsonwebtoken
+  JWTsecret: 'mysecret',
+  baseURL: baseURL,
+  port: port,
+  // The credentials and information for OAuth2
+  oauth2Credentials: {
+    client_id: "779613093885-av8jdo30b4uco1tu8nloissbkf4le17i.apps.googleusercontent.com",
+    project_id: "youtube-in-review", // The name of your project
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_secret: "TynGQZ8EZJnYKT-maqFyRJnJ",
+    redirect_uris: [
+      `${baseURL}/auth_callback`
+    ],
+    scopes: [
+      'https://www.googleapis.com/auth/youtube.readonly'
+    ]
+  }
+};
